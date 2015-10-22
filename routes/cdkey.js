afd1dd7002456f5a03f89c309a6879bbe86445aa
li24yang75 <li24yang75@163.com>
2015-10-22 01:45:49 -0700

    xxx

--- routes/cdkey.js
+++ routes/cdkey.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var Firebase = require("firebase");
 /* GET users listing. */
 router.get('/mynba2k16/verify/:keyCode/:deviceCode', function(req, res, next) {
-  //res.send('respond with a resource   ' + req.params.deviceCode);
-    var Firebase = require("firebase");
+    //res.send('respond with a resource   ' + req.params.deviceCode);
     var ref = new Firebase("https://bot11.firebaseio.com/cdkey/mynba2k16/" + req.params.keyCode);
-    ref.on("value", function(codeState) {
+    ref.once("value", function(codeState) {
         if (codeState.val() == null) {
-            res.send("无效的注册码！Invalid cdkey！");
+            res.send("无效的注册码！Invalid cdkey");
         } else {
-            if (codeState.val()["deviceCode"] != req.params.deviceCode) {
-                res.send("注册已绑定其他设备，请先解绑！This cdkey has been used by another device, please unbind firstly!");
+            if (codeState.val()["new"] == true) {
+                res.send("您的注册码尚未使用，请先绑定当前设备！Your cdkey is new，please bind with your device firstly！")
+            } else if (codeState.val()["deviceCode"] != req.params.deviceCode) {
+                res.send("注册码已绑定其他设备，请先绑定当前设备再登录！This cdkey has been used by another device, please bind cdkey with this device firstly!");
             } else {
                 var now= new Date();
                 var timeSeconds = Math.ceil(now.getTime() / 1000);
@@ -26,10 +27,48 @@ router.get('/mynba2k16/verify/:keyCode/:deviceCode', function(req, res, next) {
         //console.log(codeState.val());
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
-        res.send("error");
+        res.send("错误，请再试一次！Error, Please try again!");
     });
 });
-router.get('/mynba2k16/bind/:keyCode', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/mynba2k16/bind/:keyCode/:deviceCode', function(req, res) {
+    var ref = new Firebase("https://bot11.firebaseio.com/cdkey/mynba2k16/" + req.params.keyCode);
+    var ref1 = new Firebase("https://bot11.firebaseio.com/cdkey/mynba2k16/" + req.params.keyCode);
+    ref.once("value", function(codeState) {
+        if (codeState.val() == null) {
+            res.send("无效的注册码，绑定失败。Invalid cdkey. Faill！");
+        } else {
+            var now= new Date();
+            var timeSeconds = Math.ceil(now.getTime() / 1000);
+            if (codeState.val()["new"] == true) {
+                var validTime = timeSeconds + 31 * 24 * 60 * 60;
+                ref1.update({"deviceCode" : req.params.deviceCode, "expireTime" : validTime, "new" : false}, function(error) {
+                    if (error) {
+                        res.send("错误，请再试一次！Error, Please try again!");
+                    } else {
+                        res.send("true");
+                    }
+                });
+            } else if (codeState.val()["deviceCode"] == req.params.deviceCode) {
+                res.send("true");
+            } else {
+                if (codeState.val()["expireTime"] < timeSeconds) {
+                    res.send("注册码已经过期，请重新购买！This cdkey is expired, please buy a new one! Fail！")
+                } else {
+                    var updateValidTime = codeState.val()["expireTime"] - 4 * 60 * 60;
+                    ref1.update({"deviceCode" : req.params.deviceCode, "expireTime" : updateValidTime}, function(error) {
+                        if (error) {
+                            res.send("错误，请再试一次！Error, Please try again!");
+                        } else {
+                           res.send("true");
+                        }
+                    });
+                }
+            }
+        }
+        //console.log(codeState.val());
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        res.send("error");
+    });
 });
 module.exports = router;
