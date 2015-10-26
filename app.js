b154f30363091226855bee471c21909f7ac0f127
li24yang75 <li24yang75@163.com>
2015-10-26 16:47:13 -0700

    ss

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var cdkey = require('./routes/cdkey');
-
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
