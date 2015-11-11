be2121fc5f1399b999edcbb3dfb0cf3166f2edc9
li24yang75 <li24yang75@163.com>
2015-11-10 22:39:06 -0800

    ss

--- routes/cdkeyc.js
+++ routes/cdkeyc.js
@@ -39,6 +39,50 @@ router.get('/verify/:keyCode/:randomCode', function(req, res, next) {
 });
 
 
+
+
+router.get('/newverify/:keyCode/:deviceCode', function(req, res, next) {
+    var ref = dbmail["ref"].child('cdkey/mynba2k16/' + req.params.keyCode);
+    ref.once("value", function(codeState) {
+        if (codeState.val() == null) {
+            res.send("无效的注册码！");
+        } else {
+            var now= new Date();
+            var timeSeconds = Math.ceil(now.getTime() / 1000);
+            if (codeState.val()["new"] == true) {
+                var validTime = timeSeconds + 31 * 24 * 60 * 60;
+                ref.update({"expireTime" : validTime, "new" : false, "deviceCode" : req.params.deviceCode}, function(error) {
+                    if (error) {
+                        res.send("错误！再试一次！");
+                    } else {
+                        res.send("true");
+                    }
+                });
+            } else if (codeState.val()["expireTime"] < timeSeconds) {
+                res.send("注册码过期，请重新购买！");
+            } else if (codeState.val()["deviceCode"] != req.params.deviceCode) {
+                var validTime = codeState.val()["expireTime"] - 4 * 60 * 60;
+                ref.update({"deviceCode" : req.params.deviceCode, "expireTime" : validTime}, function(error) {
+                    if (error) {
+                        res.send("错误！再试一次！");
+                    } else {
+                        res.send("true1");
+                    }
+                });
+            } else {
+                res.send("true");
+            }
+        }
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+        res.send("错误！再试一次！");
+    });
+});
+
+
+
+
+
 router.get('/getrandomcode/:keyCode/:randomCode', function(req, res, next) {
     var ref = dbmail["ref"].child('cdkey/mynba2k16/' + req.params.keyCode +  "/randomCode");
     ref.once("value", function(codeState) {
